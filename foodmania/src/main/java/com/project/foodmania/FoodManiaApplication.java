35a5e7ee376ced5fcff89ff538d93581027fe6d8
sonammatani <sonammatani2024@gmail.com>
2024-11-05 21:54:23 +0530

    Add files via upload
    Starter project with dependencies and local db config

---
+++ foodmania/src/main/java/com/project/foodmania/FoodManiaApplication.java
@@ -0,0 +1,13 @@
+package com.project.foodmania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.project.foodmania.FoodManiaApplication.class, args);
+	}
+
+}
